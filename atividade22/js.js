ac3baa3615c17d6b5e8a36bfbf8e62c0febfed93
Juniorborgesdapaz <139707140+Juniorborgesdapaz@users.noreply.github.com>
2024-08-28 10:25:33 -0300

    pull

---
+++ atividade22/js.js
@@ -0,0 +1,11 @@
+function minhafuncao(valor) {
+    var raiz = Math.sqrt(valor);
+    alert("A raiz de " + valor + " é " + raiz);
+}
+
+function contador() {
+    var num= parseInt(prompt("digite um numero de 1 a n:"))
+
+    var resultadodiv =document.getElementById("resultado");
+    
+}
